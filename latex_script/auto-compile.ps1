befdc3ca5701bc0ef9c6d4b710792209e40342c9
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-19 16:32:20 +0800

    Prevent forcing PDF opening

--- latex_script/auto-compile.ps1
+++ latex_script/auto-compile.ps1
@@ -1,4 +1,4 @@
-$sleep_time = 5
+$sleep_time = 10
 $latex_project_name = "report"
 
 Write-Host "Clearing screen..."
@@ -7,8 +7,6 @@ Write-Host "Clear the directory..."
 Invoke-Expression "make clean"
 Write-Host "Compile for the first time..."
 Invoke-Expression "make $latex_project_name.pdf -B"
-Write-Host "Open PDF..."
-Invoke-Expression "start $latex_project_name.pdf"
 
 while (1) {
 	Write-Host "Clearing screen..."
@@ -17,6 +15,7 @@ while (1) {
 	Invoke-Expression "make $latex_project_name.pdf -B"
 	Write-Host "Available files:"
 	Invoke-Expression "ls -l"
+	Write-Host "You can view generated PDF in path: $(Get-Location)\$latex_project_name.pdf"
 	Write-Host "Sleeping for $sleep_time seconds..."
 	Start-Sleep $sleep_time
 }
