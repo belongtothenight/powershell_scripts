eea52409dad58b3f2e3066403eed5722f0d873bc
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-20 12:39:48 +0800

    Minor adjustment on text output

--- latex_script/auto-compile.ps1
+++ latex_script/auto-compile.ps1
@@ -1,30 +1,34 @@
 $sleep_time = 5
 $latex_project_name = "report"
-$output_format = @{
+$output_format_info = @{
 	ForegroundColor = "blue"
+	# BackgroundColor = "white"
+}
+$output_format_warn = @{
+	ForegroundColor = "red"
 	# BackgroundColor = "white"
 }
 
 Clear-Host
+Write-Host "Auto-compile script for LaTeX" @output_format_info
 
 while (1) {
-	Write-Host "Clearing screen..." @output_format
+	Write-Host "Clearing screen..." @output_format_info
 	Clear-Host
-	Write-Host "Auto-compile script for LaTeX" @output_format
-	Write-Host "Project name: $latex_project_name" @output_format
-	# Write-Host "Clear the directory..." @output_format
+	# Write-Host "Clear the directory..." @output_format_info
 	# Invoke-Expression "make clean"
-	Write-Host "Compiling..." @output_format
+	Write-Host "Compiling..." @output_format_info
 	Invoke-Expression "make $latex_project_name.pdf -B"
-	Write-Host "Available files:" @output_format
+	Write-Host "Available files:" @output_format_info
 	Invoke-Expression "ls -l"
-	# Write-Host "Compiling Log:" @output_format
+	# Write-Host "Compiling Log:" @output_format_info
 	# Get-Content "$latex_project_name.log"
-	Write-Host "You can view generated PDF in path: $(Get-Location)\$latex_project_name.pdf" @output_format
-	Write-Host "If you use zathura, use shift+R to reload the PDF file." @output_format
+	Write-Host "Project name: $latex_project_name" @output_format_info
+	Write-Host "You can view generated PDF in path: $(Get-Location)\$latex_project_name.pdf" @output_format_info
+	Write-Host "If you use zathura, use shift+R to reload the PDF file." @output_format_info
 	#Start-Sleep $sleep_time
 	while ($sleep_time -gt 0) {
-		Write-Host -NoNewline "`rSleeping for $sleep_time seconds..." @output_format
+		Write-Host -NoNewline "`rSleeping for $sleep_time seconds... (ctrl+c to stop)" @output_format_warn
 		Start-Sleep 1
 		$sleep_time = $sleep_time - 1
 	}
