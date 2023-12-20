fd3626ad568a506e5daec4271ab94786e4951572
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-20 16:31:34 +0800

    Add timer functionality to script

--- latex_script/auto-compile.ps1
+++ latex_script/auto-compile.ps1
@@ -9,12 +9,14 @@ $output_format_warn = @{
 	# BackgroundColor = "white"
 }
 
+$RunTime = [System.Diagnostics.Stopwatch]::StartNew()
 Clear-Host
 Write-Host "Auto-compile script for LaTeX" @output_format_info
 
 while (1) {
 	Write-Host "Clearing screen..." @output_format_info
 	Clear-Host
+	$SingleTime = [System.Diagnostics.Stopwatch]::StartNew()
 	# Write-Host "Clear the directory..." @output_format_info
 	# Invoke-Expression "make clean"
 	Write-Host "Compiling..." @output_format_info
@@ -23,8 +25,14 @@ while (1) {
 	Invoke-Expression "ls -l"
 	# Write-Host "Compiling Log:" @output_format_info
 	# Get-Content "$latex_project_name.log"
-	Write-Host "Project name: $latex_project_name" @output_format_info
-	Write-Host "You can view generated PDF in path: $(Get-Location)\$latex_project_name.pdf" @output_format_info
+	$CurrentTime = $SingleTime.Elapsed
+	$ElapsedTime = $RunTime.Elapsed
+	Write-Host "Compiling time:               $([string]::Format("{0:d2}:{1:d2}:{2:d2}", $CurrentTime.hours, $CurrentTime.minutes, $CurrentTime.seconds))" @output_format_info
+	Write-Host "Script has been running for:  $([string]::Format("{0:d2}:{1:d2}:{2:d2}", $ElapsedTime.hours, $ElapsedTime.minutes, $ElapsedTime.seconds))" @output_format_info
+	Write-Host "Project name:                 $latex_project_name" @output_format_info
+	Write-Host "PDF filepath:                 $(Get-Location)\$latex_project_name.pdf" @output_format_info
+	Write-Host "Ref filepath:                 $(Get-Location)\$latex_project_name.bib" @output_format_info # reference filename is based on latex code
+	Write-Host "Log filepath:                 $(Get-Location)\$latex_project_name.log" @output_format_info
 	Write-Host "If you use zathura, use shift+R to reload the PDF file." @output_format_info
 	#Start-Sleep $sleep_time
 	while ($sleep_time -gt 0) {
