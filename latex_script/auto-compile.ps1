2bf2e478421ad3117b2cad8aea62add1172d84d4
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-12-20 10:12:37 +0800

    Add text formating

--- latex_script/auto-compile.ps1
+++ latex_script/auto-compile.ps1
@@ -1,24 +1,30 @@
 $sleep_time = 5
 $latex_project_name = "report"
+$output_format = @{
+	ForegroundColor = "blue"
+	# BackgroundColor = "white"
+}
 
 Clear-Host
 
 while (1) {
-	Write-Host "Clearing screen..."
+	Write-Host "Clearing screen..." @output_format
 	Clear-Host
-	Write-Host "Auto-compile script for LaTeX"
-	Write-Host "Project name: $latex_project_name"
-	Write-Host "Clear the directory..."
+	Write-Host "Auto-compile script for LaTeX" @output_format
+	Write-Host "Project name: $latex_project_name" @output_format
+	Write-Host "Clear the directory..." @output_format
 	Invoke-Expression "make clean"
-	Write-Host "Compiling..."
+	Write-Host "Compiling..." @output_format
 	Invoke-Expression "make $latex_project_name.pdf -B"
-	Write-Host "Available files:"
+	Write-Host "Available files:" @output_format
 	Invoke-Expression "ls -l"
-	Write-Host "You can view generated PDF in path: $(Get-Location)\$latex_project_name.pdf"
-	Write-Host "If you use zathura, use shift+R to reload the PDF file."
+	# Write-Host "Compiling Log:" @output_format
+	# Get-Content "$latex_project_name.log"
+	Write-Host "You can view generated PDF in path: $(Get-Location)\$latex_project_name.pdf" @output_format
+	Write-Host "If you use zathura, use shift+R to reload the PDF file." @output_format
 	#Start-Sleep $sleep_time
 	while ($sleep_time -gt 0) {
-		Write-Host -NoNewline "`rSleeping for $sleep_time seconds..."
+		Write-Host -NoNewline "`rSleeping for $sleep_time seconds..." @output_format
 		Start-Sleep 1
 		$sleep_time = $sleep_time - 1
 	}
