59b922b4f0a048cd6011880e95a6d793f35590b3
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-02-05 13:31:38 +0800

    Update pandoc script content and rename folders

--- pandoc_md2pdf/generate.ps1
+++ pandoc_md2pdf/generate.ps1
@@ -1,9 +1,12 @@
 # https://stackoverflow.com/questions/18178084/pandoc-and-foreign-characters
 
-pandoc ver1.md `
+pandoc `
+  -f markdown-implicit_figures `
+  -t pdf `
+  ver1.md `
   -o pdf/ver1.pdf `
   --pdf-engine=xelatex `
   -V CJKmainfont="Microsoft JhengHei" `
-  -V geometry:margin=0.5in
+  -V geometry:margin=0.5in `
 
 pdf/ver1.pdf
